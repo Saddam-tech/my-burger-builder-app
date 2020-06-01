a7cd0654523bd534f057c890e08a822c2fd8c136
Saddam-tech <salokhiddinov0727@gmail.com>
2020-06-01 09:33:20 +0900

    more changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Aux from '../../../hoc/Aux'
 
 const orderSummary = (props) => {
 
-}
+    const ingredientSummary = props.ingrediets;
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+
+            </ul>
+        </Aux>
+    );
+
+};
 
 export default orderSummary;
