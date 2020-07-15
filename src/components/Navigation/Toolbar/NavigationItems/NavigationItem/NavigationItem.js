400ad62e2c5eb566b8658303e26bb7edaeb6d315
Saddam-tech <salokhiddinov0727@gmail.com>
2020-07-15 09:51:43 +0900

    finished the orders page

--- src/components/Navigation/Toolbar/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/Toolbar/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,10 @@
 import React from "react";
-import classes from './NavigationItem.module.css';
-
+import classes from "./NavigationItem.module.css";
+import { NavLink } from "react-router-dom";
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}> {props.children} </a>
+    <NavLink exact={props.exact} activeClassName={classes.active} to={props.link}> {props.children} </NavLink>
   </li>
 );
 
